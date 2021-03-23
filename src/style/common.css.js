ccaa3e7f383a202787a8455afdab09d0c1449f74
jeffWang <jf0423@gmail.com>
2021-03-23 22:19:35 +0800

    feat: Add the styled-components support for styling

---
+++ src/style/common.css.js
@@ -0,0 +1,9 @@
+export const eclipse = `
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+`;
+
+export const shadow = `
+    box-shadow: 1px 1px 3px 0 rgb(0 0 0 / 30%);
+`;
