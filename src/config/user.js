0e5e9f28d9171b3bd89822c32cd18d0ea40a4059
jeffWang <jf0423@gmail.com>
2021-03-23 22:21:13 +0800

    feat: Add the static user config and testing  selectors

---
+++ src/config/user.js
@@ -0,0 +1,11 @@
+export const USERS_CONFIG = {
+    NUM_COLUMNS: 4,
+    BATCH_NUM: 50,
+    MAXIMUM_COUNT: 150,
+    ITEM_WIDTH: 250,
+    ITEM_HEIGHT: 300
+};
+
+export const END_POINT = 'https://randomuser.me/api/';
+
+export const DEFAULT_NATIONALITIES = ['ch', 'es', 'fr', 'gb'];
